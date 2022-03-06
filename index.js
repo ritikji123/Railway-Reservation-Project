ee35660c3aff4c1be6305ee14c0c8a547cb7b31b
bob-loblawb <sakshamyadav751@gmail.com>
2022-03-06 09:27:30 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,39 @@
+const express = require("express");
+
+const path = require("path");
+const app = new express();
+const ejs = require("ejs");
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+
+app.listen(4000, () => {
+  console.log("App listening on port 4000");
+});
+
+app.get("/", (req, res) => {
+  res.render("index");
+});
+app.get("/about", (req, res) => {
+  //res.sendFile(path.resolve(__dirname,'pages/about.html'))
+  res.render("about");
+});
+app.get("/contact", (req, res) => {
+  //res.sendFile(path.resolve(__dirname,'pages/contact.html'))
+  res.render("contact");
+});
+app.get("/post", (req, res) => {
+  //res.sendFile(path.resolve(__dirname,'pages/post.html'))
+  res.render("post");
+});
+app.get("/booking", (req, res) => {
+  //res.sendFile(path.resolve(__dirname,'pages/post.html'))
+  res.render("booking");
+});
+app.get("/status", (req, res) => {
+  //res.sendFile(path.resolve(__dirname,'pages/post.html'))
+  res.render("status");
+});
+app.get("/pnr", (req, res) => {
+  //res.sendFile(path.resolve(__dirname,'pages/post.html'))
+  res.render("contact");
+});
